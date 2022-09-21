92d5a48a810fe8839c26cf637bc65b77f859e0ce
Jose Arreola <65381222+JoseArreolaPrado@users.noreply.github.com>
2022-09-21 16:36:31 -0500

    Script added (little before needed)

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,14 @@
+
+
+let HTML = "HTML";
+let CSS = "CSS";
+let Git = "Git";
+let JavaScript = "JavaScript";
+
+let topics = [HTML, CSS, Git, JavaScript];
+
+// console.log(topics);
+console.log(HTML);
+console.log(CSS);
+console.log(Git);
+console.log(JavaScript);
