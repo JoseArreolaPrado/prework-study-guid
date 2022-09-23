f6e407660c0325b517b96c132a3e5b707cbbba78
Jose Arreola <65381222+JoseArreolaPrado@users.noreply.github.com>
2022-09-23 10:19:48 -0500

    JavaScript code modifed with functions

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -7,11 +7,10 @@ let Git = "Git";
 let JavaScript = "JavaScript";
 
 let topics = [HTML, CSS, Git, JavaScript];
-
-
-for (let i=0; i<topics.length; i++){
-    console.log(topics[i]);
-}
+let randomTopic = topics[Math.floor(Math.random()*topics.length)];
+// for (let i=0; i<topics.length; i++){
+//     console.log(topics[i]);
+// }
 
 
 
@@ -24,17 +23,51 @@ for (let i=0; i<topics.length; i++){
 
 // var topic = "HTML";
 
-// if (topic === 'HTML') {
-//   console.log("Let's study HTML!");
-// } else if (topic === 'CSS') {
-//   console.log("Let's study CSS!");
-// } else if (topic === 'Git') {
-//   console.log("Let's study Git!");
-// } else if (topic === 'JavaScript') {
-//   console.log("Let's study JavaScript!");
-// } else {
-//   console.log('Please try again!');
-// }
+async function selectTopic(){
+    await wait (5000);
+    if (randomTopic === 'HTML') {
+      console.log("Let's study " + randomTopic + "!");
+    } else if (randomTopic === 'CSS') {
+      console.log("Let's study "  + topics[1] + "!");
+    } else if (randomTopic === 'Git') {
+      console.log("Let's study " + topics[2] + "!");
+    } else if (randomTopic === 'JavaScript') {
+      console.log("Let's study " + topics[3]+ "!");
+    } else {
+      console.log('Please try again!');
+    }
+}
 
 
 
+
+function wait(ms){
+    return new Promise(
+        r => setTimeout(r, ms)
+    );
+}
+
+
+async function  makeTea()  {
+    console.log("Boile water");
+    console.log("Add tea bag");
+    console.log("WAIT!!");
+    await wait(3000);
+    console.log("Enjoy Tea :D ");
+};
+
+// makeTea();
+
+
+async function listTopics(){
+    for ( let i = 0; i < topics.length; i++){
+        await wait(1000);
+        console.log(topics[i]);
+    }
+}
+
+console.log('Here are the topics we lerned through Prework:');
+
+listTopics();
+
+selectTopic();
